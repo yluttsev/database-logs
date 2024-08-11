5f5297768bb310b94fb501d17975737ea792b3ad
Yuri Luttsev <floomyz@vk.com>
2024-08-11 12:32:40 +0300

    init

---
+++ src/main/java/ru/luttsev/databaselogs/DatabaseLogsApplication.java
@@ -0,0 +1,13 @@
+package ru.luttsev.databaselogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseLogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseLogsApplication.class, args);
+    }
+
+}
